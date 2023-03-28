6bb9b443d4ff1e122d3885415fbf65d297e8d2f9
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:46:03 +0200

    Update scriptXodos.sh

--- scriptXodos.sh
+++ scriptXodos.sh
@@ -8,4 +8,4 @@ destino=$"$carpeta/$mes/$dia"
 
 url=$"https://dl.dropboxusercontent.com/s/fmwhpgndxefl2t7/Cristofol.jpg"
 
-wget -O $destino"/Webcam_Xodos-"$horaMinuto".jpg" $url
+wget -O $destino"/Webcam_Xodos-"$horaMinuto".jpg" $url -a /var/log/cams_$dia.log
