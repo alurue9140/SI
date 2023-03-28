bb90adaeda5ecb7b79c09dc1b80d65820d710223
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:47:08 +0200

    Update scriptPenyiscola.sh

--- scriptPenyiscola.sh
+++ scriptPenyiscola.sh
@@ -9,4 +9,4 @@ destino=$"$carpeta/$mes/$dia"
 
 url=$"https://g0.ipcamlive.com/player/snapshot.php?alias=5750a365b7d90"
 
-wget -O $destino"/Webcam_Penyiscola-"$horaMinuto".jpg" $url
+wget -O $destino"/Webcam_Penyiscola-"$horaMinuto".jpg" $url -a /var/log/cams_$dia.log
