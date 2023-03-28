a030f2f1b8b1031e03daf6530afb0e31bcfc0c15
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:13:32 +0200

    Create scriptPolop.sh

---
+++ scriptPolop.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fecha=$(date -u +"%Y%m%d%H")
+hora=$(date +"%H%M")
+mes=$(date +"%m")
+dia=$(date +"%d")
+carpeta=$"/home/equipo1/Samba-Publico/Webcam_Polop"
+destino=$"$carpeta/$mes/$dia"
+
+url1=$"https://www.avametnuvol.es/estacions/c31m107e02/polop.jpg"
+
+wget -O $destino"/Polop-"$hora".jpg" $url1
