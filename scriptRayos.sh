ec529c5ade08551f14846373e4b64f09f2b95013
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:13:07 +0200

    Create scriptRayos.sh

---
+++ scriptRayos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+fecha=$(date -u +"%Y%m%d%H")
+hora=$(date +"%H%M")
+mes=$(date +"%m")
+dia=$(date +"%d")
+carpeta=$"/home/equipo1/Samba-Publico/Rayos"
+destino=$"$carpeta/$mes/$dia"
+
+url1=$"https://www.aemet.es/imagenes_d/eltiempo/observacion/rayos/"$fecha"00_r79g.gif"
+
+wget -O $destino"/Rayos-"$hora".gif" $url1
