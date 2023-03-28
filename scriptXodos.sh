a3d22e81d75ca0705a361f434a550f07899aece0
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:10:01 +0200

    Update and rename scriptWebCamXodos.sh to scriptXodos.sh

---
+++ scriptXodos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+horaMinuto=$(date +"%H%M")
+mes=$(date +"%m")
+dia=$(date +"%d")
+
+carpeta=$"/home/equipo1/Samba-Publico/Webcam_Xodos"
+destino=$"$carpeta/$mes/$dia"
+
+url=$"https://dl.dropboxusercontent.com/s/fmwhpgndxefl2t7/Cristofol.jpg"
+
+wget -O $destino"/Webcam_Xodos-"$horaMinuto".jpg" $url
