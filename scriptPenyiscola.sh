489a4ea18d212aa9ec34888702ecb251434e28dd
Isaac <91330447+alurue9140@users.noreply.github.com>
2023-03-28 19:14:21 +0200

    Create scriptPenyiscola.sh

---
+++ scriptPenyiscola.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+fecha=$(date +"%Y%m%d%H%M%s")
+horaMinuto=$(date +"%H%M")
+mes=$(date +"%m")
+dia=$(date +"%d")
+
+carpeta=$"/home/equipo1/Samba-Publico/Webcam_Penyiscola"
+destino=$"$carpeta/$mes/$dia"
+
+url=$"https://g0.ipcamlive.com/player/snapshot.php?alias=5750a365b7d90"
+
+wget -O $destino"/Webcam_Penyiscola-"$horaMinuto".jpg" $url
